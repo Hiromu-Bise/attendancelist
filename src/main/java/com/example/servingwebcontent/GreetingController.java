393e4437e24b6dcc08dde100790f3b49bb09a5a5
ns2pole <h.bise1228@gmail.com>
2023-11-28 15:26:34 +0900

    11/28-2

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -58,7 +58,7 @@ public class GreetingController {
         Timestamp time = new Timestamp(date.getTime());
         System.out.println(time);
 
-        String sql2 = "UPDATE ATTENDANCES SET begin_time = ? WHERE id='12345678';";
+        String sql2 = "UPDATE ATTENDANCES SET begin_time = ? WHERE id='111';";
 
         jdbcTemplate.update(sql2,time);
 
