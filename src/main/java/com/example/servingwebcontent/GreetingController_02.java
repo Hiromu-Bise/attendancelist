dc1790074a1266f5ec5cc956178030792fd71d29
Hiromu-Bise <h.bise1228@gmail.com>
2023-11-29 12:02:52 +0900

    11/29

--- src/main/java/com/example/servingwebcontent/GreetingController_02.java
+++ src/main/java/com/example/servingwebcontent/GreetingController_02.java
@@ -58,8 +58,12 @@ public class GreetingController_02 {
         return "send01";
     }
     @PostMapping("/receive01") //次回ここから（send01で入力させたデータがattendandesDBのデータと合ってるか判定したい）
-    public String login(@RequestParam("id") String name,@RequestParam("pass") String pass, Model m) {
-        m.addAttribute("id",name);
+    public String receive01(
+            Model m,
+            @RequestParam("NAME") String name,
+            @RequestParam("PASS") String pass
+    ) {
+        m.addAttribute("name",name);
         m.addAttribute("pass",pass);
         return "receive01";
     }
