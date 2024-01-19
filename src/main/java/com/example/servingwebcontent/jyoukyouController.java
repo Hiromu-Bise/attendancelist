5d6adc3fe714b90a1912acb7a9436a75a914946a
HiromuBise <h.bise1228@gmail.com>
2024-01-19 13:28:56 +0900

    1/19

--- src/main/java/com/example/servingwebcontent/jyoukyouController.java
+++ src/main/java/com/example/servingwebcontent/jyoukyouController.java
@@ -6,14 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Map;
+
 public class jyoukyouController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/jyokyo")
+    @GetMapping("/jyoukyou")
     public String jyokyo(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        String sql = "SELECT * FROM ATTENDANCES;";
+        List<Map<String,Object>> attendances = jdbcTemplate.queryForList(sql);
+        System.out.println(attendances);
+        model.addAttribute("attendances", attendances);
 
-        return "jyokyo";
+        return "jyoukyou";
     }
 
 }
